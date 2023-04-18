eb5f7d46d310e67c1e659faf025eb8004905860e
Steven <stzheng716@gmail.com>
2023-04-18 15:21:11 -0700

    add SerialGenerator class

--- serial.py
+++ serial.py
@@ -17,3 +17,16 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start = 100):
+        """create a serial instance with a number"""
+        self.number = start - 1
+        self.counter = start - 1
+    
+    def generate(self):
+        """returns a new number based the class counter number"""
+        self.counter += 1
+        return self.counter
+    
+    def reset(self):
+        """resets to the instance's start number"""
+        self.counter = self.number
